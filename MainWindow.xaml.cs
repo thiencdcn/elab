93ef1a285b9f89d28792f84bdac8e8d7eb4fe7b0
thiencdcn <thiencdcn@gmail.com>
2013-08-05 12:52:01 +0700

    Tải dữ liệu lên kho chứa

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+
+namespace Check
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckMD5_OnClick(object sender, RoutedEventArgs e)
+        {
+              var openFileDialog = new OpenFileDialog
+                                     {
+                                         Title = "Open File",
+                                         Filter = "All File|*.*",
+                                         DefaultExt = "*.*"
+                                     };
+              if (openFileDialog.ShowDialog() == true)
+              {
+                  using (var fs = new FileStream(openFileDialog.FileName,FileMode.Open))
+                  {
+                    var md5 =  Convert.ToBase64String(new MD5CryptoServiceProvider().ComputeHash(fs));
+                      File.WriteAllText("E:\\Code.xml",md5);
+                      MessageBox.Show("Complete!");
+                  }
+              }
+        }
+
+        private void ButtonWriteFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+                                     {
+                                         Title = "Open File",
+                                         Filter = "All File|*.*",
+                                         DefaultExt = "*.*"
+                                     };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var stream = new FileStream(openFileDialog.FileName, FileMode.OpenOrCreate,FileAccess.ReadWrite);
+                 var length = (int) stream.Length;
+                var buf = new byte[length];
+
+                stream.Read(buf, 0, length);
+                for (var i = length; i < buf.Length; i++)
+                {
+                    buf[i] += 1;
+                }
+                stream.Position = 0;
+                stream.Write(buf, 0, length);
+
+                stream.Close();
+                
+                MessageBox.Show("Complete!");
+            }
+        }
+    }
+}
